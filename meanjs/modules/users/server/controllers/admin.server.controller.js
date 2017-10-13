82ca65570389647732420301213de8d60cdaffb3
Mario <mdmonaco89@gmail.com>
2017-10-13 13:41:37 -0300

    fixed error modificar users Admin

--- meanjs/modules/users/server/controllers/admin.server.controller.js
+++ meanjs/modules/users/server/controllers/admin.server.controller.js
@@ -21,7 +21,7 @@ exports.read = function (req, res) {
  */
 exports.update = function (req, res) {
   var user = req.model;
-  user.discipline = new Article(req.body.discipline);
+  //user.discipline = new Article(req.body.discipline);
 
 
   // For security purposes only merge these parameters
